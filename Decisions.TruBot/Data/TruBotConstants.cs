ce0a934e1b4e0bece6e8e26a6c4460c17019652a
jason@decisions.com <jason@decisions.com>
2024-03-01 14:02:08 -0500

    [INT] Deleting everything so we can get a proper PR submitted.

--- Decisions.TruBot/Data/TruBotConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace Decisions.TruBot.Data
-{
-    public class TruBotConstants
-    {
-        public const string TRUBOT_ROOT_FOLDER_ID = "01HNB8V81CT9RSWA6ZB6S9ZRT0";
-        public const string TRUBOT_ASSIGNMENT_FOLDER_ID = "TRUBOT_PROCESS_ASSIGNMENT_INTERFACE";
-        public const string TRUBOT_ASSIGNMENT_ROLE_ID = "TRUBOT_PROCESS_ASSIGNMENT_ROLE";
-    }
-}
