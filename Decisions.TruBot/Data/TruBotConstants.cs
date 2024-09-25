59b9320a0c89c1c4016eaabe919e308cc62b7b45
Khuzema <khuzema@decisions.com>
2024-09-25 16:21:51 +0530

    updating the image path

--- Decisions.TruBot/Data/TruBotConstants.cs
+++ Decisions.TruBot/Data/TruBotConstants.cs
@@ -1,9 +1,10 @@
+using DecisionsFramework.ServiceLayer;
+
 namespace Decisions.TruBot.Data
 {
     public class TruBotConstants
     {
-        internal const string TRUBOT_IMAGES_PATH = "../wwwroot/Content/CustomModuleImages/Decisions.TruBot/|trubot.svg";
-        
+        internal const string TRUBOT_IMAGES_PATH = Constants.CUSTOM_MODULE_IMAGES_LOCATION + "/Decisions.TruBot/|trubot.svg";        
         internal const string STATUS_STARTED = "Started";
         internal const string STATUS_DEPLOYING = "Deploying";
         internal const string STATUS_IN_PROGRESS = "In Progress";
